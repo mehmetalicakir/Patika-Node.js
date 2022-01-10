fbb71393838b36efa052b8afca2514616bd7e101
Mehmet Ali ÇAKIR <cakiroglu_m.ali@hotmail.com>
2022-01-10 10:24:23 +0300

    assignment 3

---
+++ assignment 3/circle.js
@@ -0,0 +1,12 @@
+function circleArea(r) {
+    console.log(Math.PI * r *r);
+}
+
+function circleCircumference(r) {
+    console.log(Math.PI * 2 * r);
+}
+
+module.exports = {
+    circleArea,
+    circleCircumference
+}
